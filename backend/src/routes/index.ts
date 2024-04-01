a7d8e392ad8c2c30bd7b347a1cc77147ab3705c9
Hoan Tran <xhoantran@gmail.com>
2024-03-31 21:31:43 -0400

    Generalize route

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { AuthRoutes } from "./auth";
+import { LocationRoutes } from "./location";
+
+
+export const routes = Router();
+
+routes.use("/api/auth", AuthRoutes);
+routes.use("/api/location", LocationRoutes);
