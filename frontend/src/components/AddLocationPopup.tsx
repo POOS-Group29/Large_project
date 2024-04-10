a5cb35e3c15634fd31e07a6c69a6ed0da09b87ca
Hoan Tran <xhoantran@gmail.com>
2024-04-10 14:52:09 -0400

    Fix build

--- frontend/src/components/AddLocationPopup.tsx
+++ frontend/src/components/AddLocationPopup.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { API } from '../services';
+import React from "react";
+import { API } from "../services";
 
 interface Props {
   onClose: () => void;
-  createCustomPoint: () => void;
 }
 
 const AddLocationPopup: React.FC<Props> = ({ onClose }) => {
@@ -19,90 +18,92 @@ const AddLocationPopup: React.FC<Props> = ({ onClose }) => {
     setLatitude(event.target.value);
   };
 
-  const handleLongitudeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleLongitudeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setLongitude(event.target.value);
   };
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-  
+
     // Check if latitude and longitude are valid numbers
     if (!isNaN(Number(latitude)) && !isNaN(Number(longitude))) {
       // Make API call to add location
-      API.location.create({
-        name: name,
-        address: "Temp",
-        city: "Temp",
-        state: "Temp",
-        zip: "temp",
-        long: parseFloat(longitude),
-        lat: parseFloat(latitude)
-      })
-      .then(response => {
-        if (response) {
-          // Location added successfully
-          console.log("Done")
-        } else {
-          // Error adding location
-          throw new Error('Failed to add location');
-        }
-      })
-      .catch(error => {
-        // Handle any errors
-        console.error("Error adding location:", error);
-        // Optionally, you can display an error message or perform error handling actions
-      })
-      .finally(() => {
-        // Close the popup regardless of success or failure
-        onClose();
-      });
+      API.location
+        .create({
+          name: name,
+          address: "Temp",
+          city: "Temp",
+          state: "Temp",
+          zip: "temp",
+          long: parseFloat(longitude),
+          lat: parseFloat(latitude),
+        })
+        .then((response) => {
+          if (response) {
+            // Location added successfully
+            console.log("Done");
+          } else {
+            // Error adding location
+            throw new Error("Failed to add location");
+          }
+        })
+        .catch((error) => {
+          // Handle any errors
+          console.error("Error adding location:", error);
+          // Optionally, you can display an error message or perform error handling actions
+        })
+        .finally(() => {
+          // Close the popup regardless of success or failure
+          onClose();
+        });
     } else {
       // Display an error message if latitude or longitude is not a number
       alert("Please enter valid latitude and longitude values.");
     }
   };
-  
 
-//   const createCustomPoint = (name, longitude, latitude) => {
-//     console.log("Get here")
-//     if (!isNaN(latitude) && !isNaN(longitude)) {
-//       setPointsData([...pointsData, { name, lat: latitude, lng: longitude }]);
-//     } else {
-//       alert("Please enter valid latitude and longitude values.");
-//     }
-//     const handleSubmit = () => {
-//       fetch("https://api.cop4331.xhoantran.com/api/location", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ longitude, latitude, name }),
-//       })
-//         .then((response) => {
-//           if (response.status === 200) {
-//             response.json().then(() => {
-//               setMessage({
-//                 message: "location added successfully",
-//                 isError: false,
-//               });
-//             });
-//           } else {
-//             response.json().then((data) => {
-//               setMessage({
-//                 message: data.message,
-//                 isError: true,
-//               });
-//             });
-//           }
-//         })
-//         .catch(() => {
-//           setMessage({
-//             message: "An error occurred",
-//             isError: true,
-//           });
-//         });
-//     };
-//   };
+  //   const createCustomPoint = (name, longitude, latitude) => {
+  //     console.log("Get here")
+  //     if (!isNaN(latitude) && !isNaN(longitude)) {
+  //       setPointsData([...pointsData, { name, lat: latitude, lng: longitude }]);
+  //     } else {
+  //       alert("Please enter valid latitude and longitude values.");
+  //     }
+  //     const handleSubmit = () => {
+  //       fetch("https://api.cop4331.xhoantran.com/api/location", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({ longitude, latitude, name }),
+  //       })
+  //         .then((response) => {
+  //           if (response.status === 200) {
+  //             response.json().then(() => {
+  //               setMessage({
+  //                 message: "location added successfully",
+  //                 isError: false,
+  //               });
+  //             });
+  //           } else {
+  //             response.json().then((data) => {
+  //               setMessage({
+  //                 message: data.message,
+  //                 isError: true,
+  //               });
+  //             });
+  //           }
+  //         })
+  //         .catch(() => {
+  //           setMessage({
+  //             message: "An error occurred",
+  //             isError: true,
+  //           });
+  //         });
+  //     };
+  //   };
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50">
@@ -110,7 +111,10 @@ const AddLocationPopup: React.FC<Props> = ({ onClose }) => {
         <h2 className="text-lg font-bold mb-4">Add Location</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
               Name
             </label>
             <input
@@ -122,7 +126,10 @@ const AddLocationPopup: React.FC<Props> = ({ onClose }) => {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="latitude" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="latitude"
+              className="block text-sm font-medium text-gray-700"
+            >
               Latitude
             </label>
             <input
@@ -134,7 +141,10 @@ const AddLocationPopup: React.FC<Props> = ({ onClose }) => {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="longitude" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="longitude"
+              className="block text-sm font-medium text-gray-700"
+            >
               Longitude
             </label>
             <input
@@ -155,7 +165,7 @@ const AddLocationPopup: React.FC<Props> = ({ onClose }) => {
             </button>
             <button
               type="submit"
-              onClick={()=>handleSubmit} // Call the createCustomPoint function when clicked
+              onClick={() => handleSubmit} // Call the createCustomPoint function when clicked
               className="px-4 py-2 bg-blue-500 text-white rounded-md"
             >
               Add
@@ -168,4 +178,3 @@ const AddLocationPopup: React.FC<Props> = ({ onClose }) => {
 };
 
 export default AddLocationPopup;
-
